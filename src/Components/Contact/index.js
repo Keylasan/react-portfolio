996da3652bd70f605b13a1e261371ba6edc9366a
Life is Now <keylasan28@yahoo.com>
2020-06-08 01:37:21 -0400

    changed contact css

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 function Contact() {
   return (
     <div class="container">
-      <h1>Let's Create Something Together</h1>
+      <h1 className="lets">Let's Create</h1>
+      <h2 className="something">something</h2>
+      <h3 className="together">together</h3>
       <div className="contact-me">
         <div className="inputs">
           <input
